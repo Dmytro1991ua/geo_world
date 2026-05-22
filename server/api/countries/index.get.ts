c9ac3924f3fd014c4054482b6f23e8877f933398
dmytro_kurchenko <dmytrokurchenko@Dmytros-MacBook-Pro.local>
2026-05-22 13:13:21 +0300

    Adjust deploy issues

--- server/api/countries/index.get.ts
+++ server/api/countries/index.get.ts
@@ -1,13 +1,11 @@
 import { mapCountryDtoToUI } from '#shared/mappers';
 import type { CountryDTO } from '#shared/types/country';
 
-export default defineEventHandler(async () => {
-  const config = useRuntimeConfig();
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig(event);
 
   const data = await fetchFromExternalApi<CountryDTO[]>(() =>
-    $fetch<CountryDTO[]>(
-      `${config.public.countriesApiBase}/all?fields=name,flags,population,region,cca2,capital,area`,
-    ),
+    $fetch<CountryDTO[]>(`${config.public.countriesApiBase}/all?fields=name,flags,population,region,cca2,capital,area`),
   );
 
   return data.map(mapCountryDtoToUI);
