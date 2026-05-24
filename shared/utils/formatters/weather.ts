9f97c21996119dc7e093e19b3f750fb3e03d2eab
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2026-05-24 11:29:08 +0300

    Fix Sunrise/Sunset unix value and UI representation (#10)

--- shared/utils/formatters/weather.ts
+++ shared/utils/formatters/weather.ts
@@ -1,13 +1,13 @@
-import { addSeconds,format, fromUnixTime } from 'date-fns';
+import { fromUnixTime } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
 
-export const formatUnixToTime = (unix: number, offsetSeconds: number): string =>
-  format(addSeconds(fromUnixTime(unix), offsetSeconds), 'HH:mm');
+export const formatUnixToTime = (unix: number, timezone: string): string =>
+  formatInTimeZone(fromUnixTime(unix), timezone, 'HH:mm');
 
-export const formatUnixToDate = (unix: number, offsetSeconds: number): string =>
-  format(addSeconds(fromUnixTime(unix), offsetSeconds), 'dd MMM yyyy');
+export const formatUnixToDate = (unix: number, timezone: string): string =>
+  formatInTimeZone(fromUnixTime(unix), timezone, 'dd MMM yyyy');
 
-export const formatUnixToDateTime = (unix: number, offsetSeconds: number): string =>
-  format(addSeconds(fromUnixTime(unix), offsetSeconds), 'dd MMM yyyy HH:mm');
+export const formatUnixToDateTime = (unix: number, timezone: string): string =>
+  formatInTimeZone(fromUnixTime(unix), timezone, 'dd MMM yyyy HH:mm');
 
-export const buildWeatherIconUrl = (icon: string): string =>
-  `https://openweathermap.org/img/wn/${icon}@2x.png`;
+export const buildWeatherIconUrl = (icon: string): string => `https://openweathermap.org/img/wn/${icon}@2x.png`;
