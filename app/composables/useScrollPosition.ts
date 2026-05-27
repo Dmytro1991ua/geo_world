f00f68373e8013e1932756b6b2c9007949e89bf2
Dmytro Kurchenko <dmitriy1991dp@gmail.com>
2026-05-27 18:54:06 +0300

    Replace custom suolution for sticky header with useWindowScroll

--- app/composables/useScrollPosition.ts
+++ app/composables/useScrollPosition.ts
@@ -1,3 +1,5 @@
+import { useWindowScroll } from '@vueuse/core';
+
 const SCROLL_THRESHOLD = 20;
 
 type UseScrollPosition = {
@@ -5,22 +7,9 @@ type UseScrollPosition = {
 };
 
 export const useScrollPosition = (): UseScrollPosition => {
-  const isHeaderScrolled = ref<boolean>(false);
+  const { y } = useWindowScroll();
 
-  const updatePosition = () => {
-    if (typeof window === 'undefined') return;
-    isHeaderScrolled.value = window.scrollY > SCROLL_THRESHOLD;
-  };
-
-  onMounted(() => {
-    window.addEventListener('scroll', updatePosition, { passive: true });
-
-    updatePosition();
-  });
-
-  onUnmounted(() => {
-    window.removeEventListener('scroll', updatePosition);
-  });
+  const isHeaderScrolled = computed(() => y.value > SCROLL_THRESHOLD);
 
   return { isHeaderScrolled };
 };
